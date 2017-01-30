35364c5dc52c483ef407b4e17c2f9a8359a38fa1
Geny <genycabral@gmail.com>
2017-01-30 00:05:41 -0500

    added functions

--- wdi-fundamentals-memorygame-master/starter-code/main.js
+++ wdi-fundamentals-memorygame-master/starter-code/main.js
@@ -1,4 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour= "king";
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+	cardElement.setAttribute('data-card', cards[i]);
+	cardElement.addEventListener('click', isTwoCards).innerHTML = '<img src="king.png" alt="King of Diamonds" />';
+
+	}
+}
+var isMatch = function () {
+
+}
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
